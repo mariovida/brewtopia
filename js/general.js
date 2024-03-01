810ed8a7a2cae445c05512f2f0cf9268bdfda547
Mario <mariovidovic97@gmail.com>
2024-03-01 08:25:19 +0100

    navigation scrolled

--- js/general.js
+++ js/general.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   var beansLeft = document.querySelector(".hero-beans_left");
   var beansRight = document.querySelector(".hero-beans_right");
+  var navigation = document.querySelector("#navigation");
 
   window.addEventListener("scroll", function () {
     if (window.scrollY > 0) {
       beansLeft.classList.add("scrolled");
       beansRight.classList.add("scrolled");
+      navigation.classList.add("scrolled");
     } else {
       beansLeft.classList.remove("scrolled");
       beansRight.classList.remove("scrolled");
+      navigation.classList.remove("scrolled");
     }
   });
 });
