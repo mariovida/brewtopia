58e57ad1c9ee2572f1e728f542341ce59569ae24
Mario <mariovidovic97@gmail.com>
2024-03-02 08:25:17 +0100

    partners section

--- index.php
+++ index.php
@@ -44,6 +44,37 @@
                     <p>Driven by a desire to elevate the ordinary into the extraordinary, we embarked on a quest to source the finest beans from around the globe. Through meticulous selection and rigorous quality control measures, we ensure that only the highest-quality beans make it into our roastery.</p>
                     <p>But our journey doesn't end there. With reverence for the artistry of coffee, our master roasters employ time-honored techniques and state-of-the-art equipment to coax out the nuanced flavors and aromas inherent in each bean. Every batch is carefully monitored and lovingly roasted to perfection, resulting in a symphony of flavors that captivates the senses.</p>
                     <p>We believe that coffee is more than just a beverage - it's a catalyst for connection, conversation, and community. That's why we're dedicated to sharing our passion for coffee with enthusiasts worldwide. Whether you're a seasoned connoisseur or a curious newcomer, we invite you to join us on this journey as we celebrate the art of coffee together.</p>
+                    <div class="about_stats d-flex justify-content-between">
+                        <div class="d-flex flex-column">
+                            <span>12+</span>
+                            Years of Experience
+                        </div>
+                        <div class="d-flex flex-column">
+                            <span>26</span>
+                            Locations Worldwide
+                        </div>
+                        <div class="d-flex flex-column">
+                            <span>97</span>
+                            Clients
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="partners">
+        <div class="wrapper">
+            <div class="row">
+                <div class="col-12">
+                    <div class="partners_logos">
+                        <img src="./images/logo1.svg">
+                        <img src="./images/logo2.svg">
+                        <img src="./images/logo3.svg">
+                        <img src="./images/logo4.svg">
+                        <img src="./images/logo5.svg">
+                        <img src="./images/logo6.svg">
+                    </div>
                 </div>
             </div>
         </div>
