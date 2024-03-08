9e321b3fe15e5508f3fee40a75a09d06e2031158
Mario <mariovidovic97@gmail.com>
2024-03-08 08:25:09 +0100

    footer init

--- inc/footer.php
+++ inc/footer.php
@@ -4,7 +4,12 @@
 <footer>
     <div class="wrapper">
         <div class="row">
-           
+            <div class="col-6">
+                <?php
+                    include 'assets/logotip.svg';
+                ?>
+            </div>
+            <div class="col-6">ABC</div>
         </div>
     </div>
 </footer>
