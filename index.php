f42cee036d6fea31e30f9a7b58527d21173f46fa
Mario <mariovidovic97@gmail.com>
2024-03-12 08:25:21 +0100

    locations section fix

--- index.php
+++ index.php
@@ -104,7 +104,7 @@
             <div class="row">
                 <div class="col-12 d-flex justify-content-between align-items-center mb-40">
                     <h3>Discover Our Cafés</h3>
-                    <a class="row-btn" href="">Locations</a>
+                    <a class="row-btn">Locations</a>
                 </div>
             </div>
             <div class="row">
