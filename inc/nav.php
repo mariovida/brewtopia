8ac0f0b0311b7af4ca2933081a91582fac97d0ad
Mario <mariovidovic97@gmail.com>
2024-03-12 08:25:19 +0100

    navigation scrolled

--- inc/nav.php
+++ inc/nav.php
@@ -11,11 +11,23 @@
                     <a href="">About</a>
                 </div>
                 <div class="d-flex justify-content-center">
-                    <a href="./"><img src="./assets/logo.svg" class="nav_logo" /></a>
+                    <a href="./" class="nav_logo">
+                        <?php
+                            include 'assets/logo.svg';
+                        ?>
+                    </a>
                 </div>
                 <div class="nav_btns-right d-flex align-items-center justify-content-end">                   
-                    <a><img src="./assets/pin.svg" /></a>
-                    <a><img src="./assets/shopping-bag.svg" /></a>
+                    <a>
+                        <?php
+                            include 'assets/pin.svg';
+                        ?>
+                    </a>
+                    <a>
+                        <?php
+                            include 'assets/shopping-bag.svg';
+                        ?>
+                    </a>
                     <a class="contact-btn">Contact</a>
                 </div>
             </div>
